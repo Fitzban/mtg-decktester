344ba4772fab8a4d14c35faa430fa4a919a16fe9
emanuele.peretti@gmail.com <emanuele.peretti@gmail.com@d84b5526-05b4-acdc-20b4-6be4b57d784f>
2014-03-24 16:11:37 +0000

    countig lost

--- trunk/MTG/Model/MTGLastTurn.vb
+++ trunk/MTG/Model/MTGLastTurn.vb
@@ -4,13 +4,14 @@
     Sub New(PV As Integer, mturnnumber As Integer)
         endingPV = PV
         pturnnumber = mturnnumber
+        If endingPV > 0 Then MyBase.lost = True
     End Sub
 
     Public endingPV As Integer
 
 
     Public Overrides Function ToString() As String
-        If endingPV > 0 Then MyBase.lost = True : Return "LOST!!!"
+        If MyBase.lost Then Return "LOST!!! " & pturnnumber.ToString & " ----- PV: " & endingPV.ToString
         Return "turn#: " + pturnnumber.ToString + " ----- PV: " + endingPV.ToString
     End Function
 
